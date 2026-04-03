18321ad30d519b57d52881a755f20018e99f1d49
uttamkumarpab-web <uttamkumarpab@gmail.com>
2026-04-03 12:22:20 +0530

    proxy change

--- proxy.js
+++ proxy.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 
-export function middleware(request) {
+export function proxy(request) {
   const url = request.nextUrl
   const pathname = url.pathname.replace(/\/$/, '')
 
